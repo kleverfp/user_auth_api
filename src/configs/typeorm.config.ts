c44ddd7c2c0676824360f682f877971477bf191d
Klever Ferreira <kleverfp@gmail.com>
2022-01-24 23:39:15 -0300

    typeorm connect pg database

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as path from 'path';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'ferreira',
+  password: 'Kfp_2008',
+  database: 'authuserdb',
+  entities: [path.resolve(__dirname,'..','db','models','*')],
+  migrations: [path.resolve(__dirname,'..','db','migrations','*')],
+  synchronize: true,
+};
